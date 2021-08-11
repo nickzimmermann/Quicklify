fe5bf33dd64687d6f5cff507386e3a9c92020737
pokemon1221 <kyokosan03@gmail.com>
2021-08-11 21:55:49 +0800

    Created a model folder

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -7,6 +7,15 @@ const ProductSchema = new mongoose.Schema(
             type: String,
             required: true
         },
+        description: {
+            type: String,
+            required: true            
+        },
+        price: {
+            type: Number,
+            required: true            
+        },
+        
     }
 )
 
