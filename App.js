9d8935f390a29689def11f67c97371f54ffcc7f9
rifkhan619 <82988779+rifkhan619@users.noreply.github.com>
2021-08-22 18:54:32 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import RegistrationScreen from './RegistrationScreen.js'
+
+import PricingHome from './PricingHome.js';
+import NavBar from './NavBar.js';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={NavBar}/>
+           <Route path="/register" exact={true} component={RegistrationScreen} />
+           <Route path="/pricing" exact={true} component={PricingHome} />
+      </Switch>
+      
+
+    </BrowserRouter>
+    
+  )
+}
+
+export default App;
