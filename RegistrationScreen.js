54533fba5aa7159356473c68c99c31249a0c1cce
Pokemon1221 <86736232+Pokemon1221@users.noreply.github.com>
2021-08-24 03:12:02 +0800

    Add files via upload

---
+++ RegistrationScreen.js
@@ -0,0 +1,13 @@
+import RegistrationForm from './RegistrationForm.js';
+import NavBar from './NavBar.js';
+
+function RegistrationScreen() {
+    return (
+        <div>
+            <NavBar logo="Logo"/>
+            <RegistrationForm />
+        </div>
+    )
+};
+
+export default RegistrationScreen;
