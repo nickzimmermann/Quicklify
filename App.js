68e7ceb0db1b3148719fd4b384a05974af2966d8
Pokemon1221 <86736232+Pokemon1221@users.noreply.github.com>
2021-08-24 02:51:37 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import RegistrationScreen from './RegistrationScreen.js'
-
+import Footer from './Footer.js';
 import PricingHome from './PricingHome.js';
 import NavBar from './NavBar.js';
+
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
            <Route path="/register" exact={true} component={RegistrationScreen} />
            <Route path="/pricing" exact={true} component={PricingHome} />
       </Switch>
-      
+      <Footer />      
 
     </BrowserRouter>
     
